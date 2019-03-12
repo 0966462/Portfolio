0272ff8caab111f7e758c857ae6216503907fd7b
Francisco Ngo <0966462@hr.nl>
2019-03-13 00:09:37 +0100

    Revert "Revert "Renamed and added some new things""
    This reverts commit f844fdc4992311c3f2ddb937eb37b4d3e64220fe.

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,15 @@ $(document).ready(function(){
       
       event.preventDefault();
 
-      var right = 1
+      var right = "<div class='projectstop'><h2>The Safezone</h2>Gemaakt met 4 andere studenten.<br>Een bordspel waarbij je het snelste naar De Safezone(midden) moet gaan.<br>Je kan items oppakken in de item spots(rode cirkels) en elke zone heeft verschillende items." +
+      "<br>Elke speler(max 4 spelers) begint in 1 van de 4 hoeken.<br>Elke zone heeft een objective item.<br>Je moet dus alle 4 de objective items hebben om naar het midden te gaan.<br>Je kan samenwerken of je kan ook elkaar aanvallen.<br>Ook heb je de kans om zombies tegen te komen.</div>" +
 
-      $(".textprojectsright").html(right);
+      "<div class='projectsleft'><h3>Speelbord</h3><img src='img/speelbord.png' alt='Speelbord' style='max-width:80%'></div>" +
+      "<div class='projectsright'><h3>Kaarten</h3></div>" +
+      "<div class='projectsbottom'><h3>Applicatie</h3>Een applicatie waarbij je:<br> - De hoeveelheid spelers kan kiezen<br> - Een dobbelsteen kan gooien<br> - Een item kaart uit een stapel kan pakken<br> - Een encounter kaart kan pakken<br> - Je stats en items kan zien<br> - De handleiding kan lezen<br> - Samen kan werken of elkaar kan aanvallen<br><br>Processing Python Mode is hierbij nodig<br>Download <a href='https://processing.org/download/' target='_blank'>hier</a> processing<br><br><b>Applicatie download</b><br><a href='files/The Safezone.zip' download>download</a></div>"
+
+
+      $(".projectsdisplay").html(right);
     });
 
     $(".2").click(function(event) {
@@ -35,7 +41,7 @@ $(document).ready(function(){
 
       var right = 2
 
-      $(".textprojectsright").html(right);
+      $(".projectsdisplay").html(right);
     });
 
     $(".3").click(function(event) {
@@ -44,7 +50,7 @@ $(document).ready(function(){
 
       var right = 3
 
-      $(".textprojectsright").html(right);
+      $(".projectsdisplay").html(right);
     });
 
     $(".4").click(function(event) {
@@ -53,16 +59,16 @@ $(document).ready(function(){
 
       var right = 4
 
-      $(".textprojectsright").html(right);
+      $(".projectsdisplay").html(right);
     });
 
     $(".5").click(function(event) {
       
       event.preventDefault();
 
-      var right = 5
+      var right = 5      
 
-      $(".textprojectsright").html(right);
+      $(".projectsdisplay").html(right);
     });
 
 
