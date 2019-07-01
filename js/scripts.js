b8dd98bda6cc06631b9703d920b4fa640ab617bd
FranciscoNgo <0966462@hr.nl>
2019-07-01 05:12:49 +0200

    Added Project B (Trip Tracker)
    + Leerdoel Jaar 1 met bewijs en reflectie

--- js/scripts.js
+++ js/scripts.js
@@ -42,18 +42,15 @@ $(document).ready(function(){
     $(".2").click(function() {
       
 
-      var right = "<div class='projectstop'><h2>Trip Tracker</h2></div>" +
-      "<div class='projectsbottom'><h4>Interviewtechnieken</h4>" +
-      "Ik had mijn zus(niet anoniem) geïnterviewd met deze vragenlijst die ik samen met mijn groepsleden heb gemaakt.<br><br><img src='img/trip tracker/interview vragenlijst.png' alt='Interview Vragenlijst' style='max-width:100%'></div>" +
-      "<div class='projectsbottom'><h4>Persona's</h4>" +
-      "Door middel van de interviews hebben we een persona gemaakt voor onze app.<br>Iedereen had eerst zijn eigen persona gemaakt(mijn persona is hieronder(incompleet)), daarna werd de beste uitgekozen(Ook hieronder).<br><h5>Eigen Persona<img src='img/trip tracker/eigen persona.png' alt='Eigen Persona' style='max-width:100%'></h5><h5>Gekozen Persona<img src='img/trip tracker/persona.png' alt='Gekozen Persona' style='max-width:100%'></h5></div>" +
-      "<div class='projectsbottom'><h4>Brainstormtechnieken</h4>" +
-      "Ik en mijn teamleden hadden afgesproken om onze eigen design te verbeteren met behulp van de brainstormtechniek Wisselstorming.<br>Hierbij had ik mijn eigen documentatie gemaakt voor de brainstormtechniek.<h5><img src='img/trip tracker/wisselstorming.png' alt='Wisselstorming' style='max-width:100%'></h5>Door middel van deze documentatie had ik mijn eigen design aangepast.<br>Ik had de kleur veranderd naar een donkere groen, omdat de text anders slecht leesbaar was.<br>Om gemakkelijk naar de volgende memory te gaan in de memory displayer, had ik de memory displayer scrollable gemaakt.<br>Zodat je niet terug hoeft te gaan om naar de volgende memory te gaan.<br>Ik had ook een nieuwe page/activity gemaakt genaamd Pick Location, waarbij je de location kan kiezen bij Create Memory en Edit Memory.<h5><img src='img/trip tracker/design 2.0.png' alt='Design' style='max-width:100%'></h5></div>" +
-      "<div class='projectsbottom'><h4>Design</h4>" +
-      "Tijdens de sprint review had de product owner mijn mock-up gekozen en de visual design van een teamlid.<h5>Visual Design van teamlid<img src='img/trip tracker/visual design teamlid.png' alt='Visual Design Teamlid' style='max-width:100%'></h5>Hierbij hadden wij 2 de visual design van de mock-up aangepast.<h5>Nieuwe Visual Design<img src='img/trip tracker/design 3.0.png' alt='Nieuwe Visual Design' style='max-width:100%'></h5></div>" +
-      "<div class='projectsbottom'><h4>Usability Testing</h4>" +
-      "Voor de eerste usability testing hadden wij de User feedback testing gekozen. Hierbij hadden wij een usability test plan opgesteld.<h5><img src='img/trip tracker/usability test plan' alt='image work in progress' style='max-width:100%'></h5>Voor de User feedback testing hadden we een vragenlijst gemaakt voor de geïnterviewde.<h5>Vragenlijst<img src='img/trip tracker/usability testing vragenlijst.png' alt='Usability Testing Vragenlijst' style='max-width:100%'></h5>Nog geen resultaten.</div>"
+      var right = "<div class='projectstop'><h2>Trip Tracker</h2>Gemaakt met 4 andere studenten.<br>Een android applicatie waar je tekst met of zonder een foto kan opslaan op een bepaalde locatie.<br>Dit noemen we een memory.<br>Een memory bestaat uit een titel, story (stukje tekst), locatie en een foto.<br><br>Hieronder is een demonstratie video over de applicatie en nog eventuele screenshots van de applicatie.</div>" + 
+      
+      "<div class='projectsleft'><h3>Demonstratie Video</h3><iframe width='320' height='180' src='https://www.youtube.com/embed/B_T7LGdNS2Y' allowfullscreen='allowfullscreen' mozallowfullscreen='mozallowfullscreen' msallowfullscreen='msallowfullscreen' oallowfullscreen='oallowfullscreen' webkitallowfullscreen='webkitallowfullscreen'></iframe></div>" +
+      "<div class='projectsright'><h3>Screenshots</h3><a id='screenshotsleft' href=''><img src='img/arrow left.png' alt='Arrow Left' style='max-width:15%; margin: 0% 0% 45% 0%'; position: fixed; top: 50%'></a><img id='screenshots' src='img/trip tracker/screenshot" + screenshots[0] + ".png' alt='screenshot " + screenshots[0] + "' style='max-width:64%; margin: 0% 3% 0% 3%'><a id='screenshotsright' href=''><img src='img/arrow right.png' alt='Arrow Right' style='max-width:15%; margin: 0% 0% 45% 0%'; position: fixed; top: 50%'></a></div>" +
 
+      "<div class='projectsbottom'><h3>Applicatie download</h3>De download is een apk file dat dus alleen werkt met android devices.<br><a href='files/TripTracker.apk' download>download</a>" + 
+      "<br><h3>Leerdoel Jaar 1</h3><a href='files/leerdoelen/Leerdoel Jaar 1.pdf' download>Leerdoel</a><br><a href='files/leerdoelen/Leerdoel Jaar 1 Bewijs en Reflectie.pdf' download>Bewijs en Reflectie</a></div>" +
+
+      "<script>$('#screenshotsleft').click(function() {func_screenshots_left(); return false}); $('#screenshotsright').click(function() {func_screenshots_right(); return false}); </script>"
 
       $(".projectsdisplay").html(right);
 
